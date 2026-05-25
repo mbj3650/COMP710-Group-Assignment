ba008ff24feeda5d7f9a28ef236c7d99ba6b4a16
MartinYan12138y <135957076+MartinYan12138y@users.noreply.github.com>
2026-05-26 01:56:51 +1200

    Add files via upload
    fixed some stuff in Tile and SceneGame
    
    Tile constructor now initializes all the members (isPath, isStart, isEnd, NextPosition, PrevPosition) so it starts more stable
    updated Tile::setPrevious to store the previous position correctly
    fixed the bounds check in MovePosition, player won't go off the screen anymore
    improved Undo so it restores pathmaker position properly (x y not swapped now)

--- SceneGame.cpp
+++ SceneGame.cpp
@@ -7,6 +7,7 @@
 #include "AnimatedSprite.h"
 #include "Pathmaker.h"
 #include "Tile.h"
+#include "Enemy.h"
 // Library includes:
 #include <cassert>
 #include "lib/imgui/imgui.h"
@@ -14,19 +15,34 @@
 #include "inlinehelpers.h"
 #include "Tilelist.h"
 #include <iostream>
+
 SceneGame::SceneGame()
 	: m_pCentre(0)
 	, m_angle(0.0f)
 	, m_rotationSpeed(1.0f)
 {
+	m_pRenderer = 0;
+	m_fSpawnTimer = 0.0f;
+	m_fTileSize = 40.0f;
+	m_iLives = 20;
 }
+
 SceneGame::~SceneGame()
 {
 	delete list;
 	delete m_pCentre;
 	delete pathmaker;
 	m_pCentre = 0;
+
+	// clean up any enemies still on screen
+	for (int i = 0; i < (int)m_enemies.size(); i++)
+	{
+		delete m_enemies[i];
+		m_enemies[i] = 0;
+	}
+	m_enemies.clear();
 }
+
 bool SceneGame::Initialise(Renderer& renderer)
 {
 	srand(time(NULL));
@@ -34,65 +50,149 @@ bool SceneGame::Initialise(Renderer& renderer)
 	const int SCREEN_HEIGHT = renderer.GetHeight();
 	list = new Tilelist();
 	pathmaker = new Pathmaker();
+	moving = true;
+	columns = SCREEN_WIDTH / 40;
+	rows = SCREEN_HEIGHT / 40;
+	list->Initialise(renderer, rows, columns); // this holds all the tiles that the map needs, including start and end
+	pathmaker->Initialise(renderer, list->Startpos); // this guy tracks the position of where the "player" is in terms of grid
 
-	columns = SCREEN_WIDTH /40;
-	rows = SCREEN_HEIGHT /40;
-	list->Initialise(renderer,rows,columns);//this holds all the tiles that the map needs, including start and end
-	pathmaker->Initialise(renderer, list->Startpos);//this guy tracks the position of where the "player" is in terms of grid
+	// store renderer so we can spawn enemies later in Process
+	m_pRenderer = &renderer;
+	// work out the actual pixel width of a tile
+	m_fTileSize = (float)SCREEN_WIDTH / (float)columns;
+
 	return true;
 }
 
 void
 SceneGame::Process(float deltaTime, InputSystem& inputSystem)
 {
+	list->Process(deltaTime); // process all tiles in lists in case they need updates or whatnot
 
-	list->Process(deltaTime);//process all tiles in lists in case they need updates or whatnot
+	if (moving)
+	{
+		// MOVEMENT FOR MAKING PATHS
+		if (inputSystem.GetKeyState(SDL_SCANCODE_W) == BS_PRESSED) // CHECK IF KEY PRESSED
+		{
+			std::cout << "w\n"; // DEBUG OUTPUT
+			MovePosition(-1, 0);
+		}
+		else if (inputSystem.GetKeyState(SDL_SCANCODE_S) == BS_PRESSED)
+		{
+			std::cout << "s\n";
+			MovePosition(1, 0);
+		}
+		else if (inputSystem.GetKeyState(SDL_SCANCODE_A) == BS_PRESSED)
+		{
+			std::cout << "a\n";
+			MovePosition(0, -1);
+		}
+		else if (inputSystem.GetKeyState(SDL_SCANCODE_D) == BS_PRESSED)
+		{
+			std::cout << "d\n";
+			MovePosition(0, 1);
+		}
 
-	//MOVEMENT FOR MAKING PATHS
-	if (inputSystem.GetKeyState(SDL_SCANCODE_W) == BS_PRESSED) {//CHECK IF KEY PRESSED
-		std::cout << "w\n";//DEBUG OUTPUT
-		if (list->GetTile({ pathmaker->pos.y,pathmaker->pos.x-1 }) != NULL) {//IF NOT NULL
-			pathmaker->pos.x -= 1;//MAKE MOVEMENT
-			list->GetTile(pathmaker->pos)->isPath = true;
+		if (inputSystem.GetKeyState(SDL_SCANCODE_B) == BS_PRESSED)
+		{
+			std::cout << "undo\n";
+			Vector2 pos = list->Undo(); // set pathmaker to previous position
+			// fixed: these were swapped before which caused the undo to jump to the wrong tile
+			pathmaker->pos.x = pos.x;
+			pathmaker->pos.y = pos.y;
 		}
 	}
-	
-	//SAME FOR THESE KEYS
-	else if (inputSystem.GetKeyState(SDL_SCANCODE_S) == BS_PRESSED) {
-		std::cout << "s\n";
-		if (list->GetTile({ pathmaker->pos.y,pathmaker->pos.x + 1 }) != NULL) {
-			pathmaker->pos.x += 1;
-			list->GetTile(pathmaker->pos)->isPath = true;
+	else
+	{
+		// path is done, start spawning enemies
+		// each enemy follows the path using the tile linked list
+
+		m_fSpawnTimer += deltaTime;
+
+		// spawn a new enemy every 1.5 seconds
+		if (m_fSpawnTimer >= 1.5f && m_pRenderer != 0)
+		{
+			m_fSpawnTimer = 0.0f;
+
+			Enemy* newEnemy = new Enemy();
+			newEnemy->Initialise(*m_pRenderer, list->GetStart(), m_fTileSize);
+			m_enemies.push_back(newEnemy);
 		}
-	}
-	
-	
-	else if (inputSystem.GetKeyState(SDL_SCANCODE_A) == BS_PRESSED) {
-		std::cout << "a\n";
-		if (list->GetTile({ pathmaker->pos.y - 1 ,pathmaker->pos.x}) != NULL) {
-			pathmaker->pos.y -= 1;
-			list->GetTile(pathmaker->pos)->isPath = true;
-		}
-	}
-	
-	else if (inputSystem.GetKeyState(SDL_SCANCODE_D) == BS_PRESSED) {
-		std::cout << "d\n";
-		if (list->GetTile({ pathmaker->pos.y + 1 ,pathmaker->pos.x }) != NULL) {
-			pathmaker->pos.y += 1;
-			list->GetTile(pathmaker->pos)->isPath = true;
+
+		// update all enemies, delete any that have reached the end
+		for (int i = (int)m_enemies.size() - 1; i >= 0; i--)
+		{
+			m_enemies[i]->Process(deltaTime);
+
+			if (m_enemies[i]->HasReachedEnd())
+			{
+				delete m_enemies[i];
+				m_enemies.erase(m_enemies.begin() + i);
+				m_iLives--;
+				std::cout << "enemy got through! lives left: " << m_iLives << "\n";
+				// TODO: game over screen when lives hit 0
+			}
 		}
 	}
 }
+
+bool SceneGame::MovePosition(int xoffset, int yoffset) // CHECKS IF GIVEN POSITION (WHEN OFFSET) IS VALID
+{
+	Vector2 position = pathmaker->pos;
+
+	// fixed: rows and columns were the wrong way around here
+	// x is the column direction so it should be bounded by columns, not rows
+	if ((position.x + xoffset >= columns)
+	||  (position.x + xoffset < 0)
+	||  (position.y + yoffset < 0)
+	||  (position.y + yoffset >= rows))
+	{
+		return false;
+	}
+
+	if (list->GetTile({ pathmaker->pos.x + xoffset, pathmaker->pos.y + yoffset })->isPath) // check if not already a path
+	{
+		return false;
+	}
+	else
+	{
+		Tile* CurrentTile = list->GetTile(pathmaker->pos); // get current tile
+		pathmaker->pos.x += xoffset; // MAKE MOVEMENT
+		pathmaker->pos.y += yoffset;
+		Tile* NextTile = list->GetTile(pathmaker->pos); // get tile player will go to
+		CurrentTile->setNext(NextTile); // set current tile's next tile to be the new position's tile
+		NextTile->setPrevious(CurrentTile); // set new tile's previous tile to be this one
+		NextTile->setPath();
+		list->path.push_back(NextTile);
+		if (list->isEnd(pathmaker->pos))
+		{
+			moving = false;
+		}
+		return true;
+	}
+}
+
 void
 SceneGame::Draw(Renderer& renderer)
 {
 	list->Draw(renderer);
+
+	// draw enemies on top of the tiles
+	for (int i = 0; i < (int)m_enemies.size(); i++)
+	{
+		m_enemies[i]->Draw(renderer);
+	}
 }
+
 void SceneGame::DebugDraw()
 {
 	ImGui::Text("Scene: Grid");
 	ImGui::InputFloat("Rotation speed", &m_rotationSpeed);
 	ImGui::SliderInt("Start row", &x, 0, rows - 1, "%d");
-	ImGui::SliderInt("Start column", &y, 0, columns-1, "%d");
-	list->GetTile( x, y )->isPath = true;
+	ImGui::SliderInt("Start column", &y, 0, columns - 1, "%d");
+	list->GetTile(x, y)->isPath = true;
+
+	// show enemy/lives info in the debug window
+	ImGui::Text("Lives: %d", m_iLives);
+	ImGui::Text("Enemies active: %d", (int)m_enemies.size());
 }
--- Tile.cpp
+++ Tile.cpp
@@ -11,8 +11,16 @@
 
 Tile::Tile()
 {
-
+	m_pSprite = 0;
+	// initialise everything so we dont get random garbage values on startup
+	isPath = false;
+	isStart = false;
+	isEnd = false;
+	Connections = 0;
+	PrevPosition = 0;
+	NextPosition = 0;
 };
+
 Tile::~Tile()
 {
 	delete m_pSprite;
@@ -21,15 +29,15 @@ Tile::~Tile()
 bool
 Tile::Initialise(Renderer& renderer, Vector2 Pos, int rows, int columns)
 {
-	Position = Pos;//set position
-	m_pSprite = renderer.CreateSprite("..\\assets\\board8x8.png");//set sprite
-	
-	m_pSprite->SetScale(//set scale to make sure it fits in screen
+	Position = Pos; // set position
+	m_pSprite = renderer.CreateSprite("..\\assets\\board8x8.png"); // set sprite
+
+	m_pSprite->SetScale( // set scale to make sure it fits in screen
 		renderer.GetWidth() / (m_pSprite->GetWidth() * (1.0 * columns))
 	);
-	m_pSprite->SetX(Position.x * m_pSprite->GetWidth() + (m_pSprite->GetWidth()/2));
+	m_pSprite->SetX(Position.x * m_pSprite->GetWidth() + (m_pSprite->GetWidth() / 2));
 	m_pSprite->SetY(Position.y * m_pSprite->GetWidth() + (m_pSprite->GetWidth() / 2));
-	
+
 	return true;
 };
 
@@ -39,38 +47,70 @@ Tile::Process(float deltaTime)
 	UpdateDraw();
 };
 
-void Tile::setStart() {
+void Tile::setStart()
+{
 	isStart = true;
 }
 
-void Tile::setEnd() {
+void Tile::setEnd()
+{
 	isEnd = true;
 }
 
+void Tile::setNext(Tile* NextPosition)
+{
+	this->NextPosition = NextPosition;
+}
+
+void Tile::setPath()
+{
+	this->isPath = true;
+}
+
+void Tile::setPrevious(Tile* PrevPosition)
+{
+	// fixed: was accidentally storing NextPosition here instead of the parameter
+	// this meant the backwards link in the path was always null
+	this->PrevPosition = PrevPosition;
+}
+
+void Tile::Undo() // reset the address of the next tile
+{
+	NextPosition->isPath = false; // remove path status from it
+	this->NextPosition = 0;
+}
+
 void
-Tile::UpdateDraw() {//changes tile color based on its status
-	if (isEnd) {
+Tile::UpdateDraw() // changes tile color based on its status
+{
+	if (isEnd)
+	{
 		m_pSprite->SetBlueTint(0);
 		m_pSprite->SetGreenTint(0);
 	}
-	else if (isStart) {
+	else if (isStart)
+	{
 		m_pSprite->SetRedTint(0);
 		m_pSprite->SetGreenTint(0);
 	}
-	else if (isPath) {
+	else if (isPath)
+	{
 		m_pSprite->SetBlueTint(0.2);
 		m_pSprite->SetRedTint(0.5);
 		m_pSprite->SetGreenTint(0.35);
 	}
-	else {
+	else
+	{
 		m_pSprite->SetBlueTint(1);
 		m_pSprite->SetRedTint(1);
 		m_pSprite->SetGreenTint(1);
 	}
-	if (static_cast<int>(Position.x + Position.y) % 2 == 0) {
+	if (static_cast<int>(Position.x + Position.y) % 2 == 0)
+	{
 		m_pSprite->SetAlpha(0.5f);
 	}
-	else {
+	else
+	{
 		m_pSprite->SetAlpha(1.0f);
 	}
 }
